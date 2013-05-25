5a8e88d0ddc4d71867364a2e119e70d309f383ce
Riton <rrrispam@gmail.com>
2013-05-25 14:25:26 -0400

    Finished static pages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,34 @@
+require 'spec_helper'
+describe "Static pages" do
+  
+  describe "Home page" do
+    
+    it "should have the content 'Sharee'" do
+      visit '/static_pages/home'
+      page.should have_content('Sharee')
+    end 
+    
+    it "should have the title 'Home'" do
+          visit '/static_pages/home'
+          page.should have_selector('title', :text => "Sharee, createurs d'experiences | Home")
+    end
+    
+  end
+
+
+  describe "About page" do
+    
+    it "should have the content 'About Us'" do
+      visit '/static_pages/about'
+      page.should have_content('About Us')
+    end 
+    
+    it "should have the title 'About'" do
+          visit '/static_pages/about'
+          page.should have_selector('title', :text => "Sharee, createurs d'experiences | About")
+    end
+    
+  end
+  
+end
+
