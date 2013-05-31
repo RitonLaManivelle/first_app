d3535ea46c55e6cb029acf0dd412b420b16c7c95
Riton <rrrispam@gmail.com>
2013-05-31 10:37:36 -0400

    First visual iteration

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,10 @@ FirstApp::Application.routes.draw do
   match '/about', to:"static_pages#about"
   match '/contact', to:"static_pages#contact"
  
-
+  resources :sessions, only: [:new, :create, :destroy]
+  match '/signin',  to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
